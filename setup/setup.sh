96e49e51351846a6b45b4a48538b99b036faaeec
mochen72 <mochen@cs.sfu.ca>
2025-04-06 21:54:23 +0000

    Make comments nicer in setup script

--- setup/setup.sh
+++ setup/setup.sh
@@ -1,19 +1,23 @@
-################
-# useful utils #
-################
+#!/bin/bash
+set -e
+
+# ======================
+# Clone non-ROS packages
+# ======================
 # tmux config
 if [ ! -f ~/.tmux.conf ]; then
     touch ~/.tmux.conf
     echo "set -g history-limit 10000" >> ~/.tmux.conf
     echo "set -g mouse on" >> ~/.tmux.conf
 fi
+
 # fzf
 if [ ! -d ~/.fzf/ ]; then
     git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
     ~/.fzf/install --key-bindings --completion --update-rc
 fi
-#################
-# hunavsim
+
+# For hunavsim
 mkdir -p py
 cd py 
 if [ ! -d lightsfm ] ; then
@@ -22,12 +26,17 @@ fi
 cd lightsfm; make; sudo make install; cd ..;
 cd ..
 
-mkdir -p src
+# ============
+# ROS packages
+# ============
+# Import repos
 vcs import < setup/ros2.repos src --skip-existing --recursive
 
-# resolve any dependancies
+# Resolve dependencies
 rosdep update
 rosdep install -i --from-path src --rosdistro humble -y
 
-# build workspace
+# Build workspace
 colcon build --symlink-install
+
+echo "Setup complete. Next, run source ~/.bashrc and follow README."
